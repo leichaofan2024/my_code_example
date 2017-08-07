797f318839baf4c0bfc0f0982e9425ed8ed9edc3
leichaofan <leichaofan2024@gmail.com>
2017-08-07 23:35:24 +0800

    create three page date

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+  get "/rich_editor" => "welcome#rich_editor"
+  get "/数据库表栏位命名冲突处理办法" => "welcome#数据库表栏位命名冲突处理办法"
+  get "/重写数据库命名惯例" => "welcome#重写数据库命名惯例"
 end
